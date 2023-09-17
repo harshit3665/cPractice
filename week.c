d837fe14d7a524ac4c56e6154db55f8b2eb3401d
harshit3665 <harshitsharma3665@gmail.com>
2023-09-17 09:23:27 +0530

    added new files

---
+++ week.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main()
+{
+    int no;
+    printf("enter the no in 1-7=");
+    scanf("%d",&no);
+    switch(no)
+    {
+        case 1:
+        printf("\nsunday");
+        break;
+
+        case 2:
+        printf("\nmonday");
+        break;
+
+        case 3:
+        printf("\ntuesday");
+        break;
+
+        case 4:
+        printf("\nwednesday");
+        break;
+
+        case 5:
+        printf("\nthursday");
+        break;
+
+        case 6:
+        printf("\nfriday");
+        break;
+         
+         case 7:
+         printf("\nsaturday");
+         break;
+         default:
+         printf("\n please enter proper no.");
+         break;
+
+    }
+
+    return 0;
+
+}
